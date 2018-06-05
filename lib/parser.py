24ec08f2b2f29c29fe25e6ec48d963c3f1bae7f7
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-05 14:08:59 +0530

    parser.py: Parser to parse command-line args

---
+++ lib/parser.py
@@ -0,0 +1,21 @@
+'''
+tuesday 5th june 2018
+'''
+
+from argparse import ArgumentParser
+from argparse import Action
+
+class Parser(ArgumentParser):
+    def __init__(self, **kwargs):
+        super(Parser, self).__init__(**kwargs)
+
+        # display version
+        self.add_argument("--version", action='store_true')
+   
+        # for configuring logging level
+        self.add_argument("-l", "--logging", default='INFO',
+            help="set logging level")
+
+        # for changing or generating config
+        self.add_argument("--config", action='store_true',
+            help="SSH remote port (defaults to 22)")
