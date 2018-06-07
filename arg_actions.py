ffcdd1c5e81c051613f83aa6b20cfb3714849a0b
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-07 14:57:54 +0530

    arg_actions.py: TODO _version desc, pep8

--- arg_actions.py
+++ arg_actions.py
@@ -23,32 +23,34 @@ FILE_PATH = os.path.join(DIR_PATH, CONFIG_FILE)
 
 # TODO: Remove port -> no port forwarding? ANDROID???
 
-"""
-Return the path of the config file
-"""
 def _get_config():
+    """
+    Return the path of the config file
+    """
     return FILE_PATH
 
-"""
-Check whether the config file exists or not
-"""
+
 def _check_config():
+    """
+    Check whether the config file exists or not
+    """
     return Path(FILE_PATH).exists()
 
-"""
-TODO: Add paths
-Looks for a 'config.ini' file in ->
-    Linux/Mac:
-    Windows:
-If the file is already present, it asks the user 
-before it overwrites the file with new configuration.
-"""
+
 def _config():
+    """
+    TODO: Add paths
+    Looks for a 'config.ini' file in ->
+        Linux/Mac:
+        Windows:
+    If the file is already present, it asks the user
+    before it overwrites the file with new configuration.
+    """
     # TODO: Absolute path for windows?
     if _check_config():
         print('A configuration file already exists.')
         print('Do you want to overwrite the existing configuration?')
-        #ans = input('[yes/no]> ')
+        # ans = input('[yes/no]> ')
         ans = 'yes'
         if ans == 'no':
             exit()
@@ -59,11 +61,11 @@ def _config():
     # SERVER settings
     print(server_config)
     print(host_msg)
-    remote_host = input('remote_host = ')       
-    
+    remote_host = input('remote_host = ')
+
     print(uid_msg)
     remote_username = input('remote_username = ')
-    
+
     print(port_msg)
     remote_port = input('remote_port = ')
 
@@ -75,12 +77,12 @@ def _config():
     print(complete_sync_msg)
     complete_sync = input('complete_sync = ')
 
-    print(local_root_msg)    
+    print(local_root_msg)
     local_root = input('local_root = ')
 
     print(remote_root_msg)
     remote_root = input('remote root = ')
-    
+
     print(ignore_msg)
     ignore_patterns = (input('ignore_patterns = '))
 
@@ -90,7 +92,7 @@ def _config():
 
     print(end_msg)
     print('\n\nSaving your configuration to \'{}\''.format(FILE_PATH))
-    
+
     # start making the config.ini file from the input
     config = ConfigParser(allow_no_value=True)
 
@@ -98,8 +100,8 @@ def _config():
     #   Every key-value pair that is of the form:
     #       Key: None
     #   are their to write the comments to the configuration file.
-    #   Just makes the conifg while easier to edit.    
-    config['SERVER'] = {server_config: None,    
+    #   Just makes the conifg while easier to edit.
+    config['SERVER'] = {server_config: None,
                         host_msg: None,
                         'remote_host': remote_host,
                         uid_msg: None,
@@ -109,7 +111,7 @@ def _config():
                         passwd_msg: None,
                         'remote_password': remote_password}
 
-    config['SYNC'] = {sync_msg: None, 
+    config['SYNC'] = {sync_msg: None,
                       local_root_msg: None,
                       'local_root': local_root,
                       ignore_msg: None,
@@ -119,13 +121,17 @@ def _config():
                       path_msg: None,
                       'local_dir': local_dir,
                       'remote_dir': remote_dir}
-    
+
     with open(FILE_PATH, 'w') as config_file:
         config.write(config_file)
     print('All done...')
 
+
 def _version():
-    author= 'Rishabh Mehta'
+    """
+    TODO: Add desc
+    """
+    author = 'Rishabh Mehta'
     email = 'eternal.blizzard23@gmail.com'
     print("{} v{}".format(SKYNET, VERSION))
     print("\n@author\n{}\n{}".format(author, email))
