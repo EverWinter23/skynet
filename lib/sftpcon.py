bc38764b94bcc34e38499c4d71c656eba40a5444
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-07 15:30:12 +0530

    sftpcon.py: Import only Connection from pysftp lib

--- lib/sftpcon.py
+++ lib/sftpcon.py
@@ -2,15 +2,17 @@
 31st may 2018 thursday
 '''
 
-import pysftp, os
+from pysftp import Connection
+import os
 import logging
+
 
 class SFTPCon:
     """
     Maintains an SSH connection to a remote server using the pysftp lib.
 
     parameters
-        host:str 
+        host:str
             the hostname or IP of remote machine
 
         username:str
@@ -31,10 +33,11 @@ class SFTPCon:
         compression:boolean
             default False, use True to enable compression
     """
-    def __init__(self, host, username = None, password = None, port = 22, 
-                 private_key_file = None, private_key_password = None,
-                 compression = False):
-        
+
+    def __init__(self, host, username=None, password=None, port=22,
+                 private_key_file=None, private_key_password=None,
+                 compression=False):
+
         self.ssh_prefix = None
         self.ssh_conn = None
 
@@ -45,7 +48,8 @@ class SFTPCon:
         if private_key_file is None:
             cnopts.hostkeys = None
 
-        logging.info('initializing connection with the following information...')
+        logging.info(
+            'initializing connection with the following information...')
         logging.info('hostname: {}'.format(host))
         logging.info('username: {}'.format(username))
         logging.info('password: {}'.format(password))
@@ -55,14 +59,16 @@ class SFTPCon:
         logging.info('compression: {}'.format(compression))
 
         self.ssh_prefix = username + '@' + password
-        
+
         if password == '':
             logging.debug('no password provided, using key auth...')
             try:
-                self.ssh_conn = pysftp.Connection(host, username=username, port=port, 
-                                                  private_key_file = private_key_file,
-                                                  private_key_password = private_key_password,
-                                                  cnopts=cnopts)
+                self.ssh_conn = Connection(host, username=username,
+                                           port=port,
+                                           private_key_file=private_key_file,
+                                           # Ignore LineLengthBear, PycodeStyleBear
+                                           private_key_password=private_key_password,
+                                           cnopts=cnopts)
 
             except Exception as error:
                 logging.debug('key auth failed...')
@@ -72,8 +78,8 @@ class SFTPCon:
 
         if self.ssh_conn is None:
             try:
-                self.ssh_conn = pysftp.Connection(host, username=username, port=port,
-                                                  password = password, cnopts=cnopts)
+                self.ssh_conn = Connection(host, username=username, port=port,
+                                           password=password, cnopts=cnopts)
             except Exception as error:
                 logging.info('failed to connect to SFTP server...')
                 logging.error('cause: {}'.format(error))
@@ -86,7 +92,7 @@ def main():
     # test settings
     import time
     start_time = time.time()
-    
+
     hostname = 'localhost'
     username = 'frost'
     password = 'finch75'
@@ -94,27 +100,22 @@ def main():
     local_base = 'local_dir'
     remote_base = 'home/frost/remote_dir'
     conn = SFTPCon(host=hostname, username=username, password=password)
-    
+
     try:
         conn.ssh_conn.put_r(local_path, remote_path)
-    except:
+    except Exception as error:
         conn.ssh_conn.mkdir(remote_path)
         conn.ssh_conn.put_r(local_path, remote_path)
 
-
     end_time = time.time()
     print('total time to transfer 12MB = {}s.'.format(end_time - start_time))
+
 
 def get_remote_path(self, local_path):
     remote_relative = local_path[self._local_base_length:]
     return self._remote_base + remote_relative
 
+
 # test module
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
