f94d5a8417ebcd4025125806ad18eff52c569ce9
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-07 15:25:47 +0530

    parser.py: Follow pep8 guidelines

--- lib/parser.py
+++ lib/parser.py
@@ -4,17 +4,18 @@ tuesday 5th june 2018
 
 from argparse import ArgumentParser
 
+
 class Parser(ArgumentParser):
     def __init__(self, **kwargs):
         super(Parser, self).__init__(**kwargs)
 
         # display version
         self.add_argument("--version", action='store_true')
-   
+
         # for configuring logging level
         self.add_argument("-l", "--logging", default='INFO',
-            help="set logging level")
+                          help="set logging level")
 
         # for changing or generating config
         self.add_argument("--config", action='store_true',
-            help="configure settings")
+                          help="configure settings")
