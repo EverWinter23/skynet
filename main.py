cfef7b1fb21a89ff221d51373a88137354eda61c
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-08 10:09:43 +0530

    main.py: Stop the daemon process --KeyboardInterrupt

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 thursday 7th june 2018
 '''
 
-import argactions
+import time
+import sys
+import arg_actions
+import logging
+from skynet import SkyNet
 from pathlib import Path
+from lib.parser import Parser
+
 
 def main():
     # parser to generate config file using command-line interface
@@ -12,13 +18,14 @@ def main():
 
     # check for config before proceeding
     if not arg_actions._check_config() and not args.config:
-        print('error: A configuration file could not be found,\n'\
-              '       please configure {} first...\n\n'.format(arg_actions.SKYNET))
+        print('error: A configuration file could not be found,\n'
+              '       please configure {} first...\n\n'.format(
+                  arg_actions.SKYNET))
         parser.print_help()
         sys.exit()
-    
+
     # actions based on parsed arguments
-    if args.config: 
+    if args.config:
         arg_actions._config()
         # exit after configuration
         sys.exit()
@@ -26,9 +33,16 @@ def main():
         arg_actions._version()
         # exit after printing version
         sys.exit()
-    
-    skynet = SkyNet(config_file=arg_action._get_config()
-                    logging_lvl=args.logging_lvl)   
+
+    skynet = SkyNet(config_file=arg_actions._get_config(),
+                    logging_lvl=args.logging)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        skynet._stop_execution()
+
 
 if __name__ == '__main__':
     main()
