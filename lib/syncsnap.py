75e2ff26ae10e51a23e7f99df7294af1c45eddcb
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-19 15:00:49 +0530

    syncsnap.py: Sync folders based on dir snapshots

---
+++ lib/syncsnap.py
@@ -0,0 +1,31 @@
+'''
+20th june 2018 tuesday
+'''
+
+import logging
+from watchdog.utils.dirsnapshot import DirectorySnapshot
+from persistqueue import FIFOSQLiteQueue as Q
+
+
+class SyncSnap():
+    """
+    TODO
+    attributes
+    _q: FFIOSQLiteQueue
+            retrieves actions stored by the wathcer in the
+            Q and executes them when the connection exists.
+    """
+
+    def __init__(self, dir_path):
+        # taking a snapshot of the current directory
+        self._snap_dir = DirectorySnapshot(dir_path)
+
+    def _first_sync(self, db_path):
+        self._q = Q(path=db_path, auto_commit=True, multithreading=True)
+        logging.info("Syncing folders already present in the dir.")
+        for path in self._snap_dir.paths:
+            logging.info("Recorded resource: {}".format(path))
+            self._q.put({'action': 'send', 'src_path': path})
+
+    def sync(self):
+        print(self.snap_dir.paths)
