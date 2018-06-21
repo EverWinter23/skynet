9b8cf1b53f5956ac3f83f2e71c8c28731b30ebea
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-21 14:06:47 +0530

    arg_actions.py: Gen config section for s3, seperate sync section

--- arg_actions.py
+++ arg_actions.py
@@ -3,6 +3,7 @@
 '''
 
 import os
+import sys
 from configparser import ConfigParser
 from pathlib import Path
 from printmsgs import *
@@ -25,6 +26,10 @@ DB_PATH = os.path.join(DIR_PATH, SKYNET_DB)
 # path for config file
 # should work for both windows as well as linux, hopefully ;-)
 FILE_PATH = os.path.join(DIR_PATH, CONFIG_FILE)
+
+# NOTE: Enter string literals for all services here
+SFTP, S3 = 'SFTP', 'S3'
+SERVICES = [SFTP, S3]
 
 
 def _get_config():
@@ -56,41 +61,68 @@ def _db_path():
     return DB_PATH
 
 
-def _config():
+def _config(service):
     """
     TODO: Add paths
     Looks for a 'config.ini' file in ->
-        Linux/Mac:
-        Windows:
+        Linux/Mac: ~/.skynet/config.ini
+        Windows: TODO
     If the file is already present, it asks the user
     before it overwrites the file with new configuration.
     """
-    # TODO: Absolute path for windows?
     if _check_config():
         print('A configuration file already exists.')
         print('Do you want to overwrite the existing configuration?')
         # ans = input('[yes/no]> ')
         ans = 'yes'
         if ans == 'no':
-            exit()
+            sys.exit()
     if not Path(DIR_PATH).exists():
         os.makedirs(DIR_PATH)
 
     print(warning)
-    # SERVER settings
-    print(server_config)
-    print(host_msg)
-    remote_host = input('remote_host = ')
+    if (service == S3):
+        _s3_config()
+    elif (service == SFTP):
+        _sftp_config()
+    _sync_config()
 
-    print(uid_msg)
-    remote_username = input('remote_username = ')
+    print(end_msg)
+    print('\n\nSaving your config to \'{}\''.format(FILE_PATH))
 
-    print(port_msg)
-    remote_port = input('remote_port = ')
 
-    print(passwd_msg)
-    remote_password = input('remote_password = ')
+def _s3_config():
+    print(s3config_msg)
+    print(bucket_msg)
+    bucket = input('bucket = ')
 
+    print(keyid_msg)
+    key_id = input('key_id = ')
+
+    print(secret_msg)
+    aws_secret = input('aws_secret = ')
+
+    print(region_msg)
+    region = (input('region = '))
+
+    # start making the config.ini file from the input
+    config = ConfigParser(allow_no_value=True)
+
+    config['S3'] = {s3config_msg: None,
+                    bucket_msg: None,
+                    'bucket': bucket,
+                    keyid_msg: None,
+                    'key_id': key_id,
+                    secret_msg: None,
+                    'aws_secret': aws_secret,
+                    region_msg: None,
+                    'region': region}
+
+    with open(FILE_PATH, 'w') as config_file:
+        config.write(config_file)
+
+
+def _sync_config():
     # SYNC settings
     print(sync_msg)
     print(complete_sync_msg)
@@ -109,26 +141,8 @@ def _config():
     local_dir = input('local dir = ')
     remote_dir = input('remote_dir = ')
 
-    print(end_msg)
-    print('\n\nSaving your configuration to \'{}\''.format(FILE_PATH))
-
-    # start making the config.ini file from the input
+    # append sync settings to config.ini file
     config = ConfigParser(allow_no_value=True)
-
-    # NOTE:
-    #   Every key-value pair that is of the form:
-    #       Key: None
-    #   are their to write the comments to the configuration file.
-    #   Just makes the conifg while easier to edit.
-    config['SERVER'] = {server_config: None,
-                        host_msg: None,
-                        'remote_host': remote_host,
-                        uid_msg: None,
-                        'remote_username': remote_username,
-                        port_msg: None,
-                        'remote_port': remote_port,
-                        passwd_msg: None,
-                        'remote_password': remote_password}
 
     config['SYNC'] = {sync_msg: None,
                       complete_sync_msg: None,
@@ -143,9 +157,45 @@ def _config():
                       'local_dir': local_dir,
                       'remote_dir': remote_dir}
 
+    with open(FILE_PATH, 'a') as config_file:
+        config.write(config_file)
+
+
+def _sftp_config():
+    # SERVER settings
+    print(sftp_config_msg)
+    print(host_msg)
+    remote_host = input('remote_host = ')
+
+    print(uid_msg)
+    remote_username = input('remote_username = ')
+
+    print(port_msg)
+    remote_port = input('remote_port = ')
+
+    print(passwd_msg)
+    remote_password = input('remote_password = ')
+
+    # start making the config.ini file from the input
+    config = ConfigParser(allow_no_value=True)
+
+    # NOTE:
+    #   Every key-value pair that is of the form:
+    #       Key: None
+    #   are their to write the comments to the configuration file.
+    #   Just makes the conifg while easier to edit.
+    config['SERVER'] = {sftp_config_msg: None,
+                        host_msg: None,
+                        'remote_host': remote_host,
+                        uid_msg: None,
+                        'remote_username': remote_username,
+                        port_msg: None,
+                        'remote_port': remote_port,
+                        passwd_msg: None,
+                        'remote_password': remote_password}
+
     with open(FILE_PATH, 'w') as config_file:
         config.write(config_file)
-    print('All done...')
 
 
 def _version():
