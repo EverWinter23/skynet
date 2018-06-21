3b5066b07a0a7637d1244591100d6a06da1ae108
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-21 14:09:14 +0530

    parser.py: Change parser args, support for aws-s3 service

--- lib/parser.py
+++ lib/parser.py
@@ -4,49 +4,53 @@ tuesday 5th june 2018
 
 from argparse import ArgumentParser
 
+# NOTE: Enter string literals for all services here
+SFTP, S3 = 'SFTP', 'S3'
+
+# parser help msgs, from here on out
+lvls = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET']
+help_log = """
+Set logging level for the logger, logging messages
+which are less severe than the "specified level will
+be ignored. Valid options are """ + ', '.join(lvls) + "."
+
+services = [SFTP, S3]
+help_with = """
+Select the remote storage service for syncing,
+supported services are """ + ', '.join(services) + "."
+
+help_config = """
+Generates config file for the specified service using cli.
+Supported services are
+""" + ', '.join(services) + "."
+
+
 class Parser(ArgumentParser):
     def __init__(self, **kwargs):
         super(Parser, self).__init__(**kwargs)
 
         # display version
         self.add_argument("--version", action='store_true',
-                          help="^ why is this lowercase? ;-)")
+                          help="^--- Why is this lowercase? ;(")
 
         # for configuring logging level
-        self.add_argument("-loglvl", default='INFO',
+        self.add_argument("--loglvl", default='INFO',
                           choices=lvls,
                           help=help_log,
-                          metavar='')
+                          metavar='LEVEL')
 
         # storage services supported
-        self.add_argument("--with",
+        self.add_argument("--run-with",
                           choices=services,
                           help=help_with,
-                          metavar='')
+                          metavar='SERVICE')
 
         # for changing or generating config
         self.add_argument("--config",
-                          help='Generate config file for the storage service.',
-                          metavar='')
+                          help=help_config,
+                          metavar='SERVICE')
 
         # for specifying a specific config file for sftp
-        self.add_argument("--sftp-file",
-                          help='Specify a sftp-config file.',
-                          metavar='')
-
-        # for specifying a specific config file for aws-s3
-        self.add_argument("--s3-file",
-                          help='Specify a aws-s3 config file.',
-                          metavar='')
-                          
-# parser help msgs, from here on out
-lvls=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET']
-help_log="""
-Set logging level for the logger, logging messages 
-which are less severe than the "specified level will 
-be ignored. Valid options are """  + ', '.join(lvls) + "."
-
-services = ['SFTP', 'AWS-S3']      
-help_with="""
-Select the remote storage service for syncing,
-supported services are """ + ', '.join(services) + "."
+        self.add_argument("--config-file",
+                          help='One file to rule them all ;p',
+                          metavar='FILE')
