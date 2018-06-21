85ba0343cfbe1a4fb83382742376238ee591cc7d
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-21 14:07:34 +0530

    printmsgs.py: Msgs for s3 config gen and config file

--- printmsgs.py
+++ printmsgs.py
@@ -7,8 +7,16 @@ wednesday 6th june 2018
 warning = """
 WARNING: We're assuming that the user knows what he's doing,\n\
 and thus there are no IO checks for empty/wrong inputs.
+However, if you're facing any issues, refer to LOG.md. And
+if you're still having a tough time, email me.
 """
-server_config = """
+
+end_msg = """
+NOTE: If you have made any mistake during the configuration,
+      you can edit this file using any text-editor or you can
+      use the cli to generate a new one for you."""
+
+sftp_config_msg = """
 # ================= Sever Configuration =================
 """
 host_msg = """# Hostname or IP address of the remote server."""
@@ -23,12 +31,30 @@ port_msg = """
 passwd_msg = """
 # SSH password for the remote server."""
 
+# S3 config
+s3config_msg = """
+# ================== S3 Configuration =================="""
+
+bucket_msg = """# Your bucket's name."""
+
+keyid_msg = """
+# Now, time for entering your aws credentials. ^_^
+# Your amazon access-key."""
+
+secret_msg = """
+# Your amazon secret-access-key."""
+
+region_msg = """
+# Enter the region. For the example given below, region = us-east-1
+# https://s3.console.aws.amazon.com/s3/home?region=us-east-1
+#                                                  ^^^^^^^^^"""
+
+
 sync_msg = """
-# ================= Sync Configuration =================')
+# ================= Sync Configuration ================='
 # Trailing slashes are optional.
 # The local path specified here should be relative to the top lvl dir.
-# Please see LOG.md for further details on configuration settings.
-"""
+# Please see LOG.md for further details on configuration settings."""
 
 complete_sync_msg = """
 # Enter boolean value, i.e. True or False [CASE SENSITIVE-- enter as is]
@@ -43,11 +69,14 @@ local_root_msg = """
 # Tilde notation is acceptable."""
 
 remote_root_msg = """
-# Top level directory location on the remote server.
-# Should be an absolute path -- without tilde notation.
+# Top level directory location on the remote storage.
+# Should be an absolute path --without tilde(~) notation.
 # Should start with leading slashes.
-# Should use UNIX-style forward slashes, since the remote server
-# will be accessed via SFTP."""
+# Should use UNIX-style forward slashes, since the remote
+# storage will be accessed using UNIX like paths.
+# NOTE: Don't get confused for s3, just specify the path of dir
+#       to which you want to sync your stuff.
+#       Example-> BUCKET_NAME/Work/Stuff [Syncs local dir to 'Stuff']"""
 
 ignore_msg = """
 # Files matching these patterns will be not be uploaded to the server.
@@ -59,8 +88,6 @@ path_msg = """
 # The following paths should not contain leading slashes.
 # local_dir should be located relative to the local_root path.
 # remote_dir should be located relative to the remote_root path.
+# INFO: Having a problem, go see LOG.md --mapping section, it'll
+#       definitely clear any doubts you have about relative mapping.
 """
-
-end_msg = """
-NOTE: If you have made any mistake during the configuration,
-      you can edit this file or use the cli to generate a new one for you."""
