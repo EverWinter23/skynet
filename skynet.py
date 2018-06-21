3d3595c74261880bba8972c9eb3c04add7d413f7
Ever Winter <eternal.blizzard23@gmail.com>
2018-06-22 01:10:04 +0530

    skynet.py: Major changes, to incorporate multiple services

--- skynet.py
+++ skynet.py
@@ -3,22 +3,23 @@ tuesday 5th june 2108
 '''
 
 import sys
+import logging
 from time import sleep
-import lib.logger as log
-from configparser import ConfigParser
-from watchdog.observers import Observer
+from pathlib import Path
+from lib.s3con import S3Con
 from lib.mapper import Mapper
+from datetime import datetime
 from lib.sftpcon import SFTPCon
 from lib.watcher import Watcher
 from lib.handler import Handler
 from lib.syncsnap import SyncSnap
-from datetime import datetime
-from pathlib import Path
+from configparser import ConfigParser
+from watchdog.observers import Observer
 # from watchdog.utils.dirsnapshot import DirectorySnapshot
 
 # string literals for convenience --DO NOT REMOVE
-SERVER = 'SERVER'
-SYNC = 'SYNC'
+SFTP, S3, SYNC = 'SFTP', 'S3', 'SYNC'
+SERVICES = [SFTP, S3]
 
 
 class SkyNet:
@@ -27,48 +28,46 @@ class SkyNet:
         config_path: str
             path to the config file
 
-        logging_lvl: str
-            sets the logging level of the logger, logging
-            messages which are less severe than level will
-            be ignored.
+        service: str
+            remote storage service, supported services
+            are S3, SFTP
 
         db_path: str
             path to the database where actions are
             stored.
     """
 
-    def __init__(self, config, logging_lvl, db_path):
-
-        # setup logging
-        self.logger = log.get_logger(log.lvl_mapping[logging_lvl])
-
+    def __init__(self, config, service, db_path):
         # get the configuration from the config file
         self.config = ConfigParser(allow_no_value=True)
         self.config.read(config)
-        self.logger.info('Parsed the config_file->{}'.format(config))
+        logging.info('Parsed the config_file->{}'.format(config))
 
         # static configuration --the mappings will not change
-        self.logger.info('Init. Mapper.')
+        logging.info('Init. Mapper.')
         self.mapper = self._get_mapper()
-        self.logger.info('Initialized Mapper.')
+        logging.info('Initialized Mapper.')
 
-        # TODO: explain first run
+        # path to database
         self.db_path = db_path
+
+        # for comparing dir snapshots
         self.syncsnap = SyncSnap(dir_path=self.mapper.local_base)
 
+        # sync files already present in the folder on first run
         if self._first_run() is not True:
-            self.logger.info('===============First Run==============')
+            logging.info('===============First Run==============')
             self.syncsnap._first_sync(db_path)
 
         # watcher will notify us of any file system events
-        self.logger.info('Init. Watcher.')
+        logging.info('Init. Watcher.')
         self.watcher = self._get_watcher()
-        self.logger.info('Initialized Watcher.')
+        logging.info('Initialized Watcher.')
 
         # observer to monitor the directory --and notify watcher
-        self.logger.info('Init. Observer thread.')
+        logging.info('Init. Observer thread.')
         self._thread_observer_ = Observer()
-        self.logger.info('Initialized Observer thread.')
+        logging.info('Initialized Observer thread.')
 
         # NOTE:
         #   Please be aware of local_root and local_base, they're DIFFERENT
@@ -85,9 +84,9 @@ class SkyNet:
         #   can be monitored --see LOG.md for details on
         #   how to do that.
         try:
-            self.logger.info('Starting _thread_observer_')
+            logging.info('Starting _thread_observer_')
             self._thread_observer_.start()
-            self.logger.info('Started _thread_observer_')
+            logging.info('Started _thread_observer_')
         except Exception as error:
             logging.info('exiting...')
             logging.info('Cause: {}'.format(error))
@@ -96,10 +95,14 @@ class SkyNet:
         # also need a handler to handle the actual transfers
         self._thread_handler_ = Handler(mapper=self.mapper, db_path=db_path)
 
-        # the ssh connection
-        self.sftpcon = None
+        # service type, one of the supported service
+        self._service_type = service
+        logging.info('Running with service {}.'.format(self._service_type))
+        # connection to storage service
+        self._con_service = None
+
         # start the daemon
-        self.logger.info('Daemon started.')
+        logging.info('Daemon started.')
         self._start_execution()
 
     def _first_run(self):
@@ -112,18 +115,18 @@ class SkyNet:
         """
         TODO: Add desc
         """
-        self.logger.info('Stopping _thread_observer_')
+        logging.info('Stopping _thread_observer_')
         self._thread_observer_.stop()
 
         if self._thread_handler_ is not None:
-            self.logger.info('Stopping _thread_handler_')
+            logging.info('Stopping _thread_handler_')
             self._thread_handler_.stop()
 
-        self.logger.info('Waiting for threads to join in.')
+        logging.info('Waiting for threads to join in.')
         self._thread_observer_.join()
         self._thread_handler_.join()
 
-        self.logger.info('Exiting gracefully.')
+        logging.info('Exiting gracefully.')
 
     def _start_execution(self):
         """
@@ -134,33 +137,45 @@ class SkyNet:
         while True:
             if self._thread_handler_._is_running is False:
                 # dynamic configuration --guard against conn drop, re-estd conn
-                self.sftpcon = self._get_connection()
+                self._con_service = self._get_connection()
+                logging.info('Connection Obtained.')
 
                 self._thread_handler_ = Handler(mapper=self.mapper,
                                                 db_path=self.db_path)
                 # init handler --to execute actions recorded by the watcher
-                self.logger.info('_thread_handler_ obtaining new connection.')
-                self._thread_handler_._schedule(con=self.sftpcon)
-                self.logger.info('Actions for handler have been shceduled.')
+                logging.info('Notifying _thread_handler_ of the connection.')
+                self._thread_handler_._schedule(con=self._con_service)
+                logging.info('Actions for handler have been shceduled.')
 
                 # try-catch seems redundant
                 try:
-                    self.logger.info('Starting _thread_handler_')
+                    logging.info('Starting _thread_handler_')
                     self._thread_handler_.start()
-                    self.logger.info('Started _thread_handler_')
+                    logging.info('Started _thread_handler_')
                 except Exception as error:
-                    self.logger.error('Cause: {}'.format(error))
-                    self.sftpcon = self._get_connection()
+                    logging.error('Cause: {}'.format(error))
+                    self._con_service = self._get_connection()
                     # need to get a new handler, this handler's done
-                    self.logger.info('_thread_handler_ halted.')
-                    self.logger.info('The scheduled actions have been paused.')
+                    logging.info('_thread_handler_ halted.')
+                    logging.info('The scheduled actions have been paused.')
             else:
-                self.logger.info('The scheduled actions are executing.')
-                self.logger.info('_exec slept->{}'.format(datetime.now()))
+                logging.info('The scheduled actions are executing.')
+                logging.info('_exec slept->{}'.format(datetime.now()))
                 sleep(10)  # if we have a handler --sleep for 5 minutes
-                self.logger.info('_exec got up->{}'.format(datetime.now()))
+                logging.info('_exec got up->{}'.format(datetime.now()))
 
     def _get_connection(self):
+        """
+        TODO: Add desc
+        """
+        logging.info('Obtaining Connection.')
+        if self._service_type == SFTP:
+            return self._get_sftp_con()
+
+        elif self._service_type == S3:
+            return self._get_s3con()
+
+    def _get_sftp_con(self):
         """
         TODO: Add desc
         """
@@ -168,22 +183,47 @@ class SkyNet:
         while True:
             try:
                 # try to connect
-                self.logger.info('Init SFTPCon.')
-                sftpcon = SFTPCon(host=self.config[SERVER]['remote_host'],
-                                  port=int(self.config[SERVER]['remote_port']),
+                logging.info('Init SFTPCon.')
+                sftpcon = SFTPCon(host=self.config[SFTP]['remote_host'],
+                                  port=int(self.config[SFTP]['remote_port']),
                                   # Ignore LineLengthBear, PyCodeStyleBear
-                                  username=self.config[SERVER]['remote_username'],
-                                  password=self.config[SERVER]['remote_password'])  # Ignore LineLengthBear, PyCodeStyleBear
-
+                                  username=self.config[SFTP]['remote_username'],
+                                  # Ignore LineLengthBear, PyCodeStyleBear
+                                  password=self.config[SFTP]['remote_password'])
                 # must've obtained the connection
-                self.logger.info('Initialized SFTPCon.')
+                logging.info('Initialized SFTPCon.')
                 return sftpcon
 
             except Exception as error:
-                self.logger.error('Cause: {}'.format(error))
-                self.logger.info('_get_con slept->{}'.format(datetime.now()))
+                logging.error('Cause: {}'.format(error))
+                logging.info('_get_con slept->{}'.format(datetime.now()))
                 sleep(5)  # check every five minutes TODO: testing for 5sec
-                self.logger.info('_get_con got up->{}'.format(datetime.now()))
+                logging.info('_get_con got up->{}'.format(datetime.now()))
+                continue
+
+    def _get_s3con(self):
+        """
+        TODO: Add desc
+        """
+        s3con = None
+        while True:
+            try:
+                # try to connect
+                logging.info('Init S3Con.')
+                s3con = S3Con(bucket_name=self.config[S3]['bucket'],
+                              key_id=self.config[S3]['key_id'],
+                              secret_key=self.config[S3]['aws_secret'],
+                              region=self.config[S3]['region'])  
+
+                # must've obtained the connection
+                logging.info('Initialized S3Con.')
+                return s3con
+
+            except Exception as error:
+                logging.error('Cause: {}'.format(error))
+                logging.info('_get_s3con slept->{}'.format(datetime.now()))
+                sleep(5)  # check every five minutes TODO: testing for 5sec
+                logging.info('_get_s3con got up->{}'.format(datetime.now()))
                 continue
 
     def _get_watcher(self):
@@ -191,9 +231,9 @@ class SkyNet:
         TODO: Add desc
         """
         # logs params passed to the Watcher --useful for debugging
-        self.logger.info('ignore_patterns={}'.format(
+        logging.info('ignore_patterns={}'.format(
             self.config[SYNC]['ignore_patterns']))
-        self.logger.info('complete_sync={}'.format(
+        logging.info('complete_sync={}'.format(
             self.config[SYNC]['complete_sync']))
 
         ignore_patterns = list(self.config[SYNC]['ignore_patterns'].split(' '))
@@ -206,13 +246,13 @@ class SkyNet:
         TODO: Add desc
         """
         # logs params passed to the Mapper --useful for debugging
-        self.logger.info('local_dir={}'.format(
+        logging.info('local_dir={}'.format(
             self.config[SYNC]['local_dir']))
-        self.logger.info('local_root={}'.format(
+        logging.info('local_root={}'.format(
             self.config[SYNC]['local_root']))
-        self.logger.info('remote_root={}'.format(
+        logging.info('remote_root={}'.format(
             self.config[SYNC]['remote_root']))
-        self.logger.info('remote_dir={}'.format(
+        logging.info('remote_dir={}'.format(
             self.config[SYNC]['remote_dir']))
 
         return Mapper(local_root=self.config[SYNC]['local_root'],
