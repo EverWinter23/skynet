9b0b84abfe83becaab910ca6b36435644e8500f8
Ever Winter <eternal.blizzard23@gmail.com>
2018-07-09 02:05:45 +0530

    main.py: Change import arg_actions->skyconf

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 '''
 thursday 7th june 2018
+command line interface
 '''
 
 import time
 import sys
 import lib.logger as log
-import arg_actions
+import skyconf
 from skynet import SkyNet
 from lib.parser import Parser
 
@@ -20,43 +21,43 @@ def main():
     logger = log.get_logger(log.lvl_mapping[args.loglvl])
 
     if args.version:
-        arg_actions._version()
+        skyconf._version()
         # exit after printing version
         sys.exit()
 
     if not args.run_with and not args.config:
         logger.info('Storage service not specified.'
                     ' Supported services are {}.'.format(
-                        ", ".join(arg_actions.SERVICES)))
+                        ", ".join(skyconf.SERVICES)))
         print('Please, specify a storage service.'
               ' Supported services are {}.'.format(
-                  ", ".join(arg_actions.SERVICES)))
+                  ", ".join(skyconf.SERVICES)))
         print('Use --run-with [SERVICE] to specify a storage service.')
         sys.exit('Exiting, check log for details.')
 
     # check for config before proceeding
-    if not arg_actions._check_config() and not args.config:
+    if not skyconf._check_config() and not args.config:
         logger.info('No config file found.'.format(
-            arg_actions.SKYNET, arg_actions.VERSION))
+            skyconf.SKYNET, skyconf.VERSION))
         print('No config found, please configure {} v{}.'.format(
-            arg_actions.SKYNET, arg_actions.VERSION))
+            skyconf.SKYNET, skyconf.VERSION))
         print('You can use --config [SERVICE] option for gen a config.')
         sys.exit('Exiting, check log for exact details.')
 
     # actions based on parsed arguments
     if args.config:
-        arg_actions._config(args.config)
+        skyconf._config(args.config)
         # exit after configuration
         sys.exit('{} v{} is ready to run.'.format(
-            arg_actions.SKYNET, arg_actions.VERSION))
+            skyconf.SKYNET, skyconf.VERSION))
 
     if args.config_file:
         logger.info('Loading the specified config.')
-        arg_actions._load_config(args.config_file)
+        skyconf._load_config(args.config_file)
 
-    skynet = SkyNet(config=arg_actions._get_config(),
+    skynet = SkyNet(config=skyconf._get_config(),
                     service=args.run_with,
-                    db_path=arg_actions.DB_PATH)
+                    db_path=skyconf.DB_PATH)
 
     try:
         while True:
