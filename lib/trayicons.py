85ce1b82fbd842ce2dc9e3590b2d5cd1b33958fd
Ever Winter <eternal.blizzard23@gmail.com>
2018-07-08 23:49:35 +0530

    trayicons.py: Icons for system tray ;-)

---
+++ lib/trayicons.py
@@ -0,0 +1,15 @@
+'''
+8th july 2018 sunday
+Just Icon names here for system tray.
+'''
+
+# NOTE: Symmetry without trying ;-) 
+SKYNET_ICON = 'skynet.svg'
+UPLOAD_ICON = 'upload.svg'
+STOP_UPLOAD = 'stop_upload.svg'
+EDIT_CONFIG = 'edit_config.svg'
+SHOW_STATUS = 'show_status.svg'
+SYNC_FOLDER = 'sync_folder.svg'
+DEBUG_ERROR = 'debug_error.svg'
+SFTP_SERVER = 'sftp_server.svg'
+EXIT_SKYNET = 'exit_skynet.svg'
