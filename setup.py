696b86a26e36f22831200a26f26fe199f49374e5
Ever Winter <eternal.blizzard23@gmail.com>
2018-07-10 15:16:14 +0530

    setup.py: alpha version v1.0.0a1 --testpypi release candidate

--- setup.py
+++ setup.py
@@ -1,26 +1,70 @@
 '''
 monday 9th june 2018
 '''
+import os
+import errno
 import setuptools
+from pathlib import Path
+
+SKYNET = 'skynet'
+SKYNET_HOME = os.path.join(str(Path.home()),
+                           SKYNET)
+PCKG = 's3kynet'
+ICONS = 'icons'
+ICON_DIR = os.path.join(PCKG, ICONS)
+
+ICON_FILES = os.listdir(ICON_DIR)
+
+try:
+    # will automatically create skynet folder
+    os.makedirs(ICON_DIR)
+except OSError as error:
+    if error.errno == errno.EEXIST and os.path.isdir(ICON_DIR):
+        pass
 
 with open("README.md", "r") as desc:
     long_description = desc.read()
 
 setuptools.setup(
     name="s3kynet",
-    version="1.6.0a1",          # marks alpha Release
+    version="1.0.0a1",          # TODO: marks --first alpha Release on pypi
     author="Rishabh Mehta",
     author_email="eternal.blizzard23@gmail.com",
     description="Sync folder to services like aws-s3, using resumable uploads.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    # long_description_content_type="text/markdown",
     url="https://github.com/EverWinter23/skynet",
     packages=setuptools.find_packages(),
     python_requires='>=3',
-    
+    license="Apache License 3.0",
+    # include_package_data=True,
+    package_data={
+        "s3kynet.icons": ICON_FILES,
+    },
+
+    install_requires=[
+        'boto3',
+        'PyQt5',
+        'pysftp',
+        'watchdog',
+        'filechunkio',
+        'persist-queue',
+        'psycopg2-binary',
+    ],
+
+    dependency_links=[
+        'https://pypi.org/project/PyQt5/',
+        'https://pypi.org/project/boto3/',
+        'https://pypi.org/project/pysftp/',
+        'https://pypi.org/project/watchdog/',
+        'https://pypi.org/project/filechunkio/',
+        'https://pypi.org/project/persist-queue/',
+        'https://pypi.org/project/psycopg2-binary/'
+    ],
+
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: AGPL-3.0-only License",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ),
 
