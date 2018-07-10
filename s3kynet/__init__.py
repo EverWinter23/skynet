8d05c95fe92c221b68a04ccdbacabfc5e8674141
Ever Winter <eternal.blizzard23@gmail.com>
2018-07-10 15:03:17 +0530

    __init__.py: Name all pkg imports

--- s3kynet/__init__.py
+++ s3kynet/__init__.py
@@ -3,3 +3,10 @@ monday 9th june 2018
 '''
 
 name = 's3kynet'
+
+from . import skylib
+from . import printmsgs
+from . import skyconf
+#from . import skymain
+from . import skynet
+from . import skytray
