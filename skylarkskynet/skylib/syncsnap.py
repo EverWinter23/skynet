d0f8ce2d90204652e1525581307ebb54eb5f5af0
Ever Winter <eternal.blizzard23@gmail.com>
2018-07-13 11:54:55 +0530

    syncsnap.py: Add docstring for the class and the method

--- skylarkskynet/skylib/syncsnap.py
+++ skylarkskynet/skylib/syncsnap.py
@@ -10,11 +10,18 @@ from persistqueue import UniqueQ as Q
 
 class SyncSnap():
     """
-    TODO
+    The SyncSnap class records all the files present in
+    the directory initially and pushes them onto the Q.
+
+    parameters
+        _dir_path: Mapper
+            the path to the dir which is to be uploaded
+            to the remote storage.
+
     attributes
-    _q: UniqueQ
-        retrieves actions stored by the wathcer in the
-        Q and executes them when the connection exists.
+        _q: UniqueQ
+            retrieves actions stored by the wathcer in the
+            Q and executes them when the connection exists.
     """
 
     def __init__(self, dir_path):
@@ -22,6 +29,18 @@ class SyncSnap():
         self._snap_dir = DirectorySnapshot(dir_path)
 
     def _first_sync(self, db_path):
+        """
+        This method records all the files present in the
+        directory initially and pushes them onto the Q.
+        It is only called once for each mapping, and never
+        again after the database for that path has been
+        established.
+
+        parameters:
+            db_path: str
+                path to the database where actions are
+                stored.
+        """
         self._q = Q(path=db_path, auto_commit=True, multithreading=True)
         logging.info("Syncing folders already present in the dir.")
         for path in self._snap_dir.paths:
